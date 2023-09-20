f48fc67d4cec350b71f71f3e584a9d7431ce5273
akarvinR <akarvin2912@gmail.com>
2023-09-20 18:32:47 -0400

    comments

--- mr/coordinator.go
+++ mr/coordinator.go
@@ -8,36 +8,85 @@ import (
 	"os"
 )
 
+/*
+Coordinator -> has 2 type of tasks [reduce/worker] ->
+
+For each worker, output the files to 10 temp files [map tasks] and pass them to the coordinator.
+
+Once, a worker had done the job, it will send a RPC to the coordinator to notify it, and ask for a new task. If no tasks are done, the coordinator will send a different call signal "none" []
+
+Once, a map task is done, the worker will send all filesnames to the coordinator.
+*/
 type Task struct {
 	workerID int
 	taskType string
 	fileName string
+	state    string
+}
+
+type CompletedTask struct {
+	task           Task
+	outputFileName string
 }
 
 type Coordinator struct {
 	// Your definitions here.
 	files       []string
 	nReduce     int
-	tasks       chan Task
-	isAlive     map[int]bool
-	tmpWorkerID int
+	mapTasks    Task
+	reduceTasks chan Task
+	tmpWorkerID int //TODO MAKE ATOMIC
+	workers     map[int]bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
 func (c *Coordinator) GetTask(args struct{}, reply *Task) error {
-	// Your code here.
-	// c.isAlive[c.tmpWorkerID] = true
-	c.tmpWorkerID++
+
+	//if mapworkers are completed
 	task := <-c.tasks
 	reply.fileName = task.fileName
 	reply.taskType = task.taskType
 	reply.workerID = c.tmpWorkerID
+	//clean all workers.
+
+
+	//if only reduce works are left
+	//
 	return nil
 }
+func (c *Coordinator) taskCompleted(args struct{}, reply struct{}) {
+	//put tasksinprogress to taskscompleted.
+	//remove worker from workers, and from reduce jobs.
 
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
+
+}
+
+func (c *Coordinator) removeWorker(args struct{}, reply struct{}) {
+	//remove tasks from the tasksinprogress and put it in taskstobedone
+	//
+
+}
+func (c *Coordinator) waitForHeartBeat(args struct{}, reply struct{})  {
+	//store alive count
+	//sleepfor5seconds
+	//if alivenotincreamend
+	//killworker
+
+}
+func (c *Coordinator) heartBeat(args struct{}, reply struct{}) error {
+	//increament alive count
+	//sleepfor10seconds
+	//go waitforheartbeat//10 seconds
+	//sendreply
+
+	//return
+
+}
+
+// rpc done task
+func (c *Coordinator) DoneTask(args *Task, reply *struct{}) error {
+	// Your code here.
+}
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
